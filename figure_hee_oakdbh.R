a4cec6ed9e63e836b652ab27d452b2d23070461b
Ken Kellner <ken.kellner@gmail.com>
2015-01-15 13:27:21 -0500

    Update to HEE figure colors.

--- figure_hee_oakdbh.R
+++ figure_hee_oakdbh.R
@@ -7,8 +7,8 @@ data.sm = as.numeric(read.csv('hee_oakdbh.csv',header=FALSE)[,2])
 
 #Plot histograms of dbh for oak vs. sugar maple
 par(mfrow=c(2,1),mar=c(4,4,3,2))
-hist(data.bo, breaks=15,xlim=c(0,40),main="Black Oak", col='gray', xlab="DBH")
+hist(data.bo, breaks=15,xlim=c(0,40),main="Black Oak", col=rgb(red=75,green=142,blue=26, maxColorValue=255), xlab="DBH")
 abline(v=median(data.bo,na.rm=TRUE),lwd=3)
-hist(data.sm, breaks=15,xlim=c(0,40), main="Sugar Maple", col='gray28', xlab="DBH")
+hist(data.sm, breaks=15,xlim=c(0,40), main="Sugar Maple", col=rgb(red=244,green=125,blue=66, maxColorValue=255), xlab="DBH")
 abline(v=median(data.sm),lwd=3)
 
