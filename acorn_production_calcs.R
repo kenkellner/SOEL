7880dac0d55f9faec83329f82a14676f6cc9a03c
Ken Kellner <ken.kellner@gmail.com>
2015-03-13 14:46:31 -0400

    Calculate means / make figures for black oak production. Work on some ideas for masting schedule.

--- acorn_production_calcs.R
+++ acorn_production_calcs.R
@@ -48,6 +48,43 @@ hist(unlist(wo[,2:10]/0.34),freq=FALSE,col='gray',main='All Years Distribution',
 lines(0:200,dexp(0:200,lambda.all),col='red',lwd=3)
 text(100,0.02,expression(lambda*" = 0.08518"),cex=2)
 
+####BO
+
+#Mast year
+hist(unlist(bo[,c(3,6,8,10)])/0.34)
+
+lambda.mast = 1/mean(unlist(bo[,c(3,6,8,10)]/0.34),na.rm=TRUE)
+
+#lambda = 0.06030
+
+hist(unlist(bo[,c(3,6,8,10)]/0.34),freq=FALSE,col='gray',main='Mast Year Distribution',
+     xlab=expression(Acorns~per~m^{2}~canopy),xlim=c(0,200))
+lines(0:200,dexp(0:200,lambda.mast),col='red',lwd=3)
+text(100,0.02,expression(lambda*" = 0.06030"),cex=2)
+
+#Non-mast year
+hist(unlist(wo[,c(4,5)])/0.34)
+lambda.nonmast = 1/mean(unlist(wo[,c(4,5)]/0.34),na.rm=TRUE)
+
+#lambda = 0.26672
+
+hist(unlist(bo[,c(4,5)]/0.34),freq=FALSE,col='gray',main='Mast Failure Year Distribution',
+     xlab=expression(Acorns~per~m^{2}~canopy),xlim=c(0,200))
+lines(0:200,dexp(0:200,lambda.nonmast),col='red',lwd=3)
+text(100,0.2,expression(lambda*" = 0.26672"),cex=2)
+
+#Mean across years
+
+hist(unlist(bo[,2:10])/0.34)
+lambda.all = 1/mean(unlist(bo[,2:10]/0.34),na.rm=TRUE)
+
+#lambda = 0.09033
+
+hist(unlist(bo[,2:10]/0.34),freq=FALSE,col='gray',main='All Years Distribution',
+     xlab=expression(Acorns~per~m^{2}~canopy),xlim=c(0,200))
+lines(0:200,dexp(0:200,lambda.all),col='red',lwd=3)
+text(100,0.02,expression(lambda*" = 0.09033"),cex=2)
+
 #Deciding when to mast
 
 X = 4
@@ -57,4 +94,13 @@ beta = 1
 
 p <- 1 / (1 + (X/alpha) ^ beta)
 p
+
+out <- vector(length=10)
+out[1] = 0
+start = 0.25
+for (i in 2:10){
+  out[i] <- rbinom(1,1,start)
+  if (out [i] == 0){start = start + 0.25}
+  if (out [i] == 1){start = 0.25}
+}
 
