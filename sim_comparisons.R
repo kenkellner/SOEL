c514e1b2f33afc1f0626963d18163ced5b881f13
Ken Kellner <ken.kellner@gmail.com>
2015-05-26 20:32:01 +0000

    Add some figures and placeholders for future simulation control function.

--- sim_comparisons.R
+++ sim_comparisons.R
@@ -119,10 +119,38 @@ noseedlings.out <- val.sim(4,150,model.path,seedlings=FALSE,heegrowth=NULL)
 #Seedlings, HEE growth data
 seedlings.out <- val.sim(4,150,model.path,seedlings=TRUE,heegrowth=TRUE)
 #Seedlings, literature growth data
-seedlings.out <- val.sim(4,150,model.path,seedlings=TRUE,heegrowth=FALSE)
+seedlingslit.out <- val.sim(4,150,model.path,seedlings=TRUE,heegrowth=FALSE)
 
+plot(rowMeans(jabowa.out$clear$BA),type='l',col='red',xlab="Year",ylab="Basal Area",lwd=2,
+     main="Clearcut")
+lines(rowMeans(noseedlings.out$clear$BA),type='l',col='orange',lwd=2)
+lines(rowMeans(seedlingslit.out$clear$BA),type='l',col='green',lwd=2)
+lines(rowMeans(seedlings.out$clear$BA),type='l',col='blue',lwd=2)
+legend("bottomright",col=c('red','orange','green','blue'), lwd=2,
+       legend=c('JABOWA','NoSeedlings','LitValues','HEEValues'))
 
+plot(rowMeans(jabowa.out$none$BA),type='l',col='red',xlab="Year",ylab="Basal Area",lwd=2,
+     main="No Harvest")
+lines(rowMeans(noseedlings.out$none$BA),type='l',col='orange',lwd=2)
+lines(rowMeans(seedlingslit.out$none$BA),type='l',col='green',lwd=2)
+lines(rowMeans(seedlings.out$none$BA),type='l',col='blue',lwd=2)
+legend("right",col=c('red','orange','green','blue'), lwd=2,
+       legend=c('JABOWA','NoSeedlings','LitValues','HEEValues'))
 
+plot(rowMeans(jabowa.out$single$BA),type='l',col='red',xlab="Year",ylab="Basal Area",lwd=2,
+     main="Single Tree")
+lines(rowMeans(noseedlings.out$single$BA),type='l',col='orange',lwd=2)
+lines(rowMeans(seedlingslit.out$single$BA),type='l',col='green',lwd=2)
+lines(rowMeans(seedlings.out$single$BA),type='l',col='blue',lwd=2)
+legend("right",col=c('red','orange','green','blue'), lwd=2,
+       legend=c('JABOWA','NoSeedlings','LitValues','HEEValues'))
 
+plot(rowMeans(jabowa.out$shelter$BA),type='l',col='red',xlab="Year",ylab="Basal Area",lwd=2,
+     main="Shelterwood")
+lines(rowMeans(noseedlings.out$shelter$BA),type='l',col='orange',lwd=2)
+lines(rowMeans(seedlingslit.out$shelter$BA),type='l',col='green',lwd=2)
+lines(rowMeans(seedlings.out$shelter$BA),type='l',col='blue',lwd=2)
+legend("right",col=c('red','orange','green','blue'), lwd=2,
+       legend=c('JABOWA','NoSeedlings','LitValues','HEEValues'))
 
 
