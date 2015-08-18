29878c4c704fcb75d6322fcf35f8427bf27acbcc
Ken Kellner <ken.kellner@gmail.com>
2015-08-18 17:22:30 +0000

    Prepare for first experiment

--- experiment_masting.R
+++ experiment_masting.R
@@ -1,10 +1,35 @@
 
 source('sim_function.R')
 
-mast.average <- forest.sim(xcorewidth=50, ycorewidth=50, nreps=2,
-                    burnin=5,nyears=10,
+mast.average <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+                    burnin=20,nyears=40,
                     mast.scenario='fixedaverage')
+save(mast.average,file='output/mast_average.Rdata')
+mast.good <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+                           burnin=20,nyears=40,
+                           mast.scenario='fixedgood')
+save(mast.good,file='output/mast_good.Rdata')
+mast.bad <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+                        burnin=20,nyears=40,
+                        mast.scenario='fixedbad')
+save(mast.bad,file='output/mast_bad.Rdata')
+mast.hee <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+                       burnin=20,nyears=40,
+                       mast.scenario='hee')
+save(mast.hee,file='output/mast_hee.Rdata')
+mast.random <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+                       burnin=20,nyears=40,
+                       mast.scenario='random')
+save(mast.random,file='output/mast_random.Rdata')
+mast.priorgood <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+                       burnin=20,nyears=40,
+                       mast.scenario='priorgood')
+save(mast.priorgood,file='output/mast_priorgood.Rdata')
+mast.priorbad <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+                       burnin=20,nyears=40,
+                       mast.scenario='priorbad')
+save(mast.priorbad,file='output/mast_priorbad.Rdata')
 
 
 library(RPushbullet)
-pbPost('note','testing','testing more text',devices='Nexus 6')
+pbPost('note','Analysis Complete','Simple mast analysis on rbrutus32 complete',devices='Nexus 6')
