ed1e26af9313a7b4dc359374cfd56a8cad012958
Ken Kellner <ken.kellner@gmail.com>
2015-08-20 16:03:00 -0400

    Add utility functions for summarizing model output.

---
+++ utility_functions.R
@@ -0,0 +1,78 @@
+
+#function to combine scenario datasets and re-sort by treatment
+treat.reorg <- function(datalist){
+  
+  out <- list()
+  
+  for (i in 1:(length(datalist[[1]])-1)){
+    
+    hold <- list()
+    for (j in 1:length(datalist)){
+      hold[[j]] <- datalist[[j]][[i]]
+      
+    }
+    names(hold) <- names(datalist)
+    out[[i]] <- hold
+    
+    
+  }
+  names(out) <- names(datalist[[1]])[1:length(names(datalist[[1]]))-1]
+  
+  return(out)
+}
+
+#Function to summarize data from a particular year and metric
+gen.dataset <- function(datalist,metric,year){
+  
+  temp <- treat.reorg(datalist)
+  
+  out <- as.data.frame(matrix(data=NA,
+                        nrow=length(temp)*length(temp[[1]])*dim(temp[[1]][[1]][[1]])[2]
+                        ,ncol=4))
+                        
+  names(out) <- c(metric,'harvest','scenario','rep')
+  
+  nreps <- dim(temp[[1]][[1]][[1]])[2]
+  
+  out[,4] <- rep(c(1:nreps),length(temp)*length(temp[[1]]))
+  
+  index=1
+  
+  for (i in 1:length(temp)){
+    
+    for (j in 1:length(temp[[1]])){
+      
+      dat.add <- eval(parse(text=paste('temp[[i]][[j]]$',metric,'[',year,',]',sep="")))
+      temp.harv <- rep(names(temp)[i],length(dat.add))
+      temp.treat <- rep(names(temp[[1]])[j],length(dat.add))
+      out[index:(index+length(dat.add)-1),1:3] <- cbind(dat.add,temp.harv,temp.treat)
+      index = index + length(dat.add)
+    }
+  }
+  return(out)
+}
+
+gen.figures <- function(datalist,metric,year,maxy){
+  
+  dat <- gen.dataset(datalist,metric,year)
+  
+  for (i in 1:length(unique(dat$harvest))){
+    temp <- dat[dat$harvest == unique(dat$harvest)[i],]
+    
+    means <- sds <- vector(length = length(unique(temp$scenario)))
+    
+    for (j in 1:length(unique(temp$scenario))){
+      
+      hold <- as.numeric(temp[temp$scenario==unique(temp$scenario)[j],1])
+      means[j] <- mean(hold)
+      sds[j] <- sd(hold)
+    }
+      
+    brp <- barplot(means,ylab=metric,xlab="Scenario",names=unique(temp$scenario),
+                    main=unique(dat$harvest)[i],
+                    ylim=c(0,maxy))
+      for (k in 1:length(means)){
+          segments(brp[k],means[k]-sds[k],brp[k],means[k]+sds[k])
+    }
+  }
+}
