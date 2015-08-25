78682c5cc792c8f7dfcbc2bdac5c64fe109bd80d
Ken Kellner <ken.kellner@gmail.com>
2015-08-25 20:37:06 +0000

    Update browse experiment to new parameter values.

--- experiment_browse.R
+++ experiment_browse.R
@@ -7,46 +7,53 @@ library(RPushbullet)
 start.time <- Sys.time()
 
 #Run experiment and save results
-browse.hee <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+browse.hee <- forest.sim(xcorewidth=140, ycorewidth=140, nreps=24,
                             burnin=20,nyears=40,
                             browse.scenario = "hee",
-                            force.processors = 10)
+                            force.processors = 12,
+                         ram.max = 5000)
 save(browse.hee,file='output/browse_hee.Rdata')
-browse.prob0 <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+browse.prob0 <- forest.sim(xcorewidth=140, ycorewidth=140, nreps=24,
                          burnin=20,nyears=40,
                          browse.scenario = "custom",
                          prob.browsed = 0,
-                         force.processors = 10)
+                         force.processors = 12,
+                         ram.max = 5000)
 save(browse.prob0,file='output/browse_prob0.Rdata')
-browse.prob2 <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+browse.prob2 <- forest.sim(xcorewidth=140, ycorewidth=140, nreps=24,
                            burnin=20,nyears=40,
                            browse.scenario = "custom",
                            prob.browsed = 0.2,
-                           force.processors = 10)
+                           force.processors = 12,
+                           ram.max = 5000)
 save(browse.prob2,file='output/browse_prob2.Rdata')
-browse.prob4 <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+browse.prob4 <- forest.sim(xcorewidth=140, ycorewidth=140, nreps=24,
                            burnin=20,nyears=40,
                            browse.scenario = "custom",
                            prob.browsed = 0.4,
-                           force.processors = 10)
+                           force.processors = 12,
+                           ram.max = 5000)
 save(browse.prob4,file='output/browse_prob4.Rdata')
-browse.prob6 <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+browse.prob6 <- forest.sim(xcorewidth=140, ycorewidth=140, nreps=24,
                            burnin=20,nyears=40,
                            browse.scenario = "custom",
                            prob.browsed = 0.6,
-                           force.processors = 10)
+                           force.processors = 12,
+                           ram.max = 5000)
 save(browse.prob6,file='output/browse_prob6.Rdata')
-browse.prob8 <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+browse.prob8 <- forest.sim(xcorewidth=140, ycorewidth=140, nreps=24,
                            burnin=20,nyears=40,
                            browse.scenario = "custom",
                            prob.browsed = 0.8,
-                           force.processors = 10)
+                           force.processors = 12,
+                           ram.max = 5000)
 save(browse.prob8,file='output/browse_prob8.Rdata')
-browse.prob10 <- forest.sim(xcorewidth=200, ycorewidth=200, nreps=30,
+browse.prob10 <- forest.sim(xcorewidth=140, ycorewidth=140, nreps=24,
                            burnin=20,nyears=40,
                            browse.scenario = "custom",
                            prob.browsed = 1.0,
-                           force.processors = 10)
+                           force.processors = 12,
+                           ram.max = 5000)
 save(browse.prob10,file='output/browse_prob10.Rdata')
 
 #Calculate runtime and push alert message
