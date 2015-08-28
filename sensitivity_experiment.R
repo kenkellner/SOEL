090e32f9b8e957f3518af5d1ac6facdd9f50b2bb
Ken Kellner <ken.kellner@gmail.com>
2015-08-28 12:59:06 -0400

    Add alert code for sensitivity experiment.

--- sensitivity_experiment.R
+++ sensitivity_experiment.R
@@ -1,7 +1,18 @@
 
 source('sensitivity_test.R')
 
+library(RPushbullet)
+
+start.time <- Sys.time()
+
 sens.test <- sensitivity.test(nreps=96,burnin=20,length=26,
                          harvests = c('none','clearcut'),
                          force.processors=12, ram.max=5000)
 save(sens.test,file='output/sens_test.Rdata')
+
+#Calculate runtime and push alert message
+end.time <- Sys.time() 
+runtime <- round(as.numeric(end.time-start.time,units="mins"),digits=3)
+pbPost('note','Analysis Complete',
+       paste('Sensitivity experiment on rbrutus16 complete after',runtime,'minutes. Shutting down instance.'),
+       devices='Nexus 6')
